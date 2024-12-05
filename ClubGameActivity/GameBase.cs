e7d544ce67fb1e49b2d5813906262932730635ad
JasonLoh <lohj002@gmail.com>
2024-12-05 10:43:18 -0500

    Add project files.

---
+++ ClubGameActivity/GameBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Diagnostics;
+
+namespace ClubGameActivity
+{
+    public partial class GameBase : Game
+    {
+        Color BackgroundColor = Color.CornflowerBlue;
+        Texture2D _texture;
+        const string url = "https://www.wabash.edu/images2/technology/canvas.png";
+
+        void Initalize()
+        {
+            WindowWidth = 1280;
+            WindowHeight = 720;
+
+            _texture = FromWeb(url);
+        }
+
+        void Update()
+        {
+            //this method is run 60x a second
+            Debug.WriteLine("Update called!");
+        }
+
+        void Draw()
+        {
+            //this is run right after "Update"
+            _spriteBatch.Draw(_texture, Vector2.Zero, Color.White);
+        }
+    }
+}
---
+++ ClubGameActivity/IgnoreThisFile.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubGameActivity
+{
+    public partial class GameBase : Game
+    {
+        public static void Main()
+        {
+            using var game = new GameBase();
+            game.Run();
+        }
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private HttpClient _httpClient;
+
+        private int WindowWidth { get => _graphics.PreferredBackBufferWidth; set => _graphics.PreferredBackBufferWidth = value; }
+        private int WindowHeight { get => _graphics.PreferredBackBufferHeight; set => _graphics.PreferredBackBufferHeight = value; }
+
+        public GameBase()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            _httpClient = new HttpClient();
+        }
+
+        protected override void Initialize()
+        {
+            Initalize();
+            base.Initialize();
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _graphics.ApplyChanges();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            Update();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(BackgroundColor);
+            _spriteBatch.Begin();
+            Draw();
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+
+        public Texture2D FromWeb(string url)
+        {
+            var s = _httpClient.GetStreamAsync(url).Result;
+            return Texture2D.FromStream(GraphicsDevice, s);
+        }
+    }
+}
